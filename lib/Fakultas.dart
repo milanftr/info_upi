381f27bda7f83c942c229837761cfe2073cf4985
milanftr <101059236+milanftr@users.noreply.github.com>
2022-05-20 17:36:54 +0700

    Update Fakultas.dart

--- lib/Fakultas.dart
+++ lib/Fakultas.dart
@@ -10,18 +10,58 @@ class Fakultas extends StatelessWidget {
       child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
         InkWell(
           child: Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Text("baris 1"),
-          ),
+              decoration: BoxDecoration(border: Border.all()),
+              padding: EdgeInsets.all(14),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          width: 100,
+                          padding: EdgeInsets.all(8),
+                          child: Image.network(
+                              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg')),
+                      Container(
+                        child: Text("FPMIPA",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                      ),
+                      Container(
+                        child: Text(
+                            "Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam",
+                            style: TextStyle(fontSize: 12)),
+                      ),
+                    ],
+                  )),
           onTap: () {
             //gunakan navigator untuk panggil RincianFakultas
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RincianFakultas()),
+            );
           },
         ),
         Container(
             decoration: BoxDecoration(border: Border.all()),
             padding: EdgeInsets.all(14),
-            child: Text("baris kedua")),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: 100,
+                    padding: EdgeInsets.all(8),
+                    child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg')),
+                Container(
+                  child: Text("FPIPS",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                ),
+                Container(
+                  child: Text("Fakultas Pendidikan Ilmu Pengatahuan Sosial",
+                      style: TextStyle(fontSize: 12)),
+                ),
+              ],
+            )),
       ]),
     );
   }
