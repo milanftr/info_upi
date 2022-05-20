182820e9a9e8ef04321b5242a8b6605db7d529a0
milanftr <101059236+milanftr@users.noreply.github.com>
2022-05-20 17:37:20 +0700

    Update Fasilitas.dart

--- lib/Fasilitas.dart
+++ lib/Fasilitas.dart
@@ -1,11 +1,69 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class Fasilitas extends StatelessWidget {
   const Fasilitas({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('fasilitas!'),
+    return Center(
+      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
+        InkWell(
+          child: Container(
+              decoration: BoxDecoration(border: Border.all()),
+              padding: EdgeInsets.all(14),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 100,
+                      padding: EdgeInsets.all(8),
+                      child: Image.network(
+                          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg')),
+                  Container(
+                    child: Text("Gymnasium",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              )),
+        ),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: 100,
+                    padding: EdgeInsets.all(8),
+                    child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg')),
+                Container(
+                  child: Text("Kolam Renang",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                ),
+              ],
+            )),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: 100,
+                    padding: EdgeInsets.all(8),
+                    child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg')),
+                Container(
+                  child: Text("Asrama Mahasiswa",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                ),
+              ],
+            )),
+      ]),
     );
   }
 }
